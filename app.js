242c485787e9c7e9d4d6313fa90eb01629677ff4
ctaggart27 <ctaggart27@gmail.com>
2017-05-24 16:52:29 -0600

    peekaboo

--- app.js
+++ app.js
@@ -13,17 +13,13 @@ const contributors = require('./routes/contributors')
 
 const app = express()
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'hbs')
-
 // uncomment after placing your favicon in /public
 // app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'client')))
 
 app.use('/', index)
 app.use('/users', users)
