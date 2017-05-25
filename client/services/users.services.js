3bd716e7044fb66440b383dfda109341945309ed
ctaggart27 <ctaggart27@gmail.com>
2017-05-25 16:23:08 -0600

    RIGTH ON DUDE

--- client/services/users.services.js
+++ client/services/users.services.js
@@ -3,7 +3,7 @@
 
   angular.module('app')
     .service('UsersServices', service)
-  service.$inject = ['$http', '$stateParams', '$state', '$filter']
+  service.$inject = ['$http']
 
   function service($http) {
 
@@ -12,50 +12,33 @@
     // }
 
     this.$User = function(id) { // Grab a user by ID
-      return $http.get(`/users/${id}`).then(one => one.data)
+      return $http.get(`/api/users/${id}`).then(one => one.data)
     }
 
     this.createUser = function(createUser) { // Makes new user
-      $http.user('/users', createUser)
+      $http.user('/api/users', createUser)
       $state.reload();
     }
 
     this.$del = function(id) { // Removes User by ID
-      $http.delete(`/users/${id}`).then(d => $state.go('app.all'))
+      $http.delete(`/api/users/${id}`).then(d => $state.go('app.all'))
     }
 
     this.$like = function(user, dir) { // Handles both likes and dislikes
       user.negative = false // Makes my error span pop out atcha
       dir == true ?
-        $http.user(`/users/${user.id}/votes`)
+        $http.user(`/api/users/${user.id}/votes`)
         .then(likes => user.vote_count = likes.data.vote_count) :
         (dir == false && user.vote_count > 0) ?
-        $http.delete(`/users/${user.id}/votes`)
+        $http.delete(`/api/users/${user.id}/votes`)
         .then(dislikes => user.vote_count = dislikes.data.vote_count) :
         user.negative = !user.negative // makes my error pop out if they try and go past 0
     }
 
     this.edit = function(user) { // Patches current User by ID
-      $http.patch(`/users/${id}/`, user)
+      $http.patch(`/api/users/${id}/`, user)
       $state.go('app.all')
     }
-
-    //THIS IS BROKEN ATM
-
-    // let sortUsers
-    // this.sort = function(sortOrder) {
-    //   sortUsers = sortOrder
-    //   console.log(sortUsers);
-    // }
-    // this.sorted = function() {
-    //   console.log(sortUsers);
-    //   return sortUsers
-    //   return $http.get('/users').then((all) => {
-    //     return $filter('orderBy')(all.data, sortUsers)
-    //   }).then((sorted) => {
-    //     return sorted
-    //   })
-    // }
 
   }
 })();
