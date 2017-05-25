faa2cf6c82e4225c0ed70e6a0c9a56376fe959d4
Haley Kalb <haley.kalb@gmail.com>
2017-05-24 19:34:41 -0600

    migrations

--- migrations/20170524190407_posts.js
+++ migrations/20170524190407_posts.js
@@ -2,9 +2,9 @@ exports.up = function(knex) {
   return knex.schema.createTable('posts', (table) => {
     table.increments()
     table.string('title').notNullable()
-    table.integer('user_id').notNullable().references('users.id').onDelete('CASCADE')
+    table.integer('user_id').references('users.id').onDelete('CASCADE')
     table.text('description').defaultTo('')
-    table.integer('pose_id').notNullable().references('poses.id').onDelete('CASCADE')
+    table.integer('pose_id').references('poses.id').onDelete('CASCADE')
     table.text('post_img').notNullable().defaultTo('')
     table.text('post_video').defaultTo('')
     table.timestamps(true, true)
--- migrations/20170524190446_comments.js
+++ migrations/20170524190446_comments.js
@@ -4,8 +4,8 @@ exports.up = function(knex) {
     table.boolean('verified').defaultTo(false)
     table.text('comment_text').notNullable().defaultTo('')
     table.text('comment_img').defaultTo('')
-    table.integer('user_id').notNullable().references('users.id').onDelete('CASCADE')
-    table.integer('post_id').notNullable().references('posts.id').onDelete('CASCADE')
+    table.integer('user_id').references('users.id').onDelete('CASCADE')
+    table.integer('post_id').references('posts.id').onDelete('CASCADE')
     table.timestamps(true, true)
   })
 }
