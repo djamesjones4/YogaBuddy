dfaa6c4584c5faadbaca95deedfb32bd3b76372a
Haley Kalb <haley.kalb@gmail.com>
2017-05-24 18:17:41 -0600

    migrations

--- migrations/20170524141943_comments.js
+++ migrations/20170524141943_comments.js
@@ -1,10 +1,18 @@
 exports.up = function(knex) {
   return knex.schema.createTable('comments', (table) => {
     table.increments();
+    table.boolean('verified').defaultTo(FALSE);
     table.string('varchar').notNullable().defaultTo('');
     table.string('email').notNullable().unique();
     table.specificType('hashed_password', 'char(60)').notNullable();
     table.timestamps(true, true);
+
+    comment_text = models.TextField()
+    comment_img = models.TextField(blank=True, null=True)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
   });
 };
 
