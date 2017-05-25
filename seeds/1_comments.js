a04073dedd4d41103173d798b57a8e0a91fb809b
Haley Kalb <haley.kalb@gmail.com>
2017-05-24 19:28:42 -0600

    users, posts and comments seeds

--- seeds/1_comments.js
+++ seeds/1_comments.js
@@ -4,14 +4,48 @@
             .then(function() {
               return Promise.all([
                 // Inserts seed entries
-                knex('comments').insert([{
-                  id: 1,
-                  varchar: 'Rowling',
-                  email: 'jkrowling@gmail.com',
-                  hashed_password: 'cptcptcptcptcptcptcptcptcptcptcptcptcptcptcptcptcptcptcptcpt',
-                  created_at: new Date('2016-06-29 14:26:16 UTC'),
-                  updated_at: new Date('2016-06-29 14:26:16 UTC')
-                }])
+                knex('comments').insert([
+                    {
+                        id: 1,
+                        verified: false,
+                        comment_text : "Keep pulling your shoulders apart, and don't allow your lower back to sag down. Also, the shoulder blades typically want to dip in the middle - don't let that happen. Keep pulling the shoulder blades outward.",
+                        comment_img : "http://lovemyyoga.com/images/Upward%20Dog.jpg",
+                        created_at : new Date('2016-06-29 14:26:16 UTC'),
+                        updated_at : new Date('2016-06-29 14:26:16 UTC')
+                    },
+                    {
+                        id: 2,
+                        verified: false,
+                        comment_text : "SAMPLE text here.",
+                        comment_img : "http://lovemyyoga.com/images/Upward%20Dog.jpg",
+                        created_at : new Date('2016-06-29 14:26:16 UTC'),
+                        updated_at : new Date('2016-06-29 14:26:16 UTC')
+                    },
+                    {
+                        id: 3,
+                        verified: true,
+                        comment_text : "SAMPLE text here.",
+                        comment_img : "http://lovemyyoga.com/images/Upward%20Dog.jpg",
+                        created_at : new Date('2016-06-29 14:26:16 UTC'),
+                        updated_at : new Date('2016-06-29 14:26:16 UTC')
+                    },
+                    {
+                        id: 4,
+                        verified: true,
+                        comment_text : "SAMPLE text here.",
+                        comment_img : "http://lovemyyoga.com/images/Upward%20Dog.jpg",
+                        created_at : new Date('2016-06-29 14:26:16 UTC'),
+                        updated_at : new Date('2016-06-29 14:26:16 UTC')
+                    },
+                    {
+                        id: 5,
+                        verified: false,
+                        comment_text : "SAMPLE text here.",
+                        comment_img : "http://lovemyyoga.com/images/Upward%20Dog.jpg",
+                        created_at : new Date('2016-06-29 14:26:16 UTC'),
+                        updated_at : new Date('2016-06-29 14:26:16 UTC')
+                    }
+                  ])
               ]);
             }).then(() => {
               return knex.raw("SELECT setval('comments_id_seq', (SELECT MAX(id) FROM comments));")
--- seeds/3_posts.js
+++ seeds/3_posts.js
@@ -4,14 +4,35 @@
             .then(function() {
               return Promise.all([
                 // Inserts seed entries
-                knex('posts').insert([{
-                  id: 1,
-                  varchar: 'Rowling',
-                  email: 'jkrowling@gmail.com',
-                  hashed_password: 'cptcptcptcptcptcptcptcptcptcptcptcptcptcptcptcptcptcptcptcpt',
-                  created_at: new Date('2016-06-29 14:26:16 UTC'),
-                  updated_at: new Date('2016-06-29 14:26:16 UTC')
-                }])
+                knex('posts').insert([
+                  {
+                    id: 1,
+                    title: "Back Pain",
+                    description : "I would like some advice on how to align my back in this pose. I am experiencing pain in my shoulders. Can someone help?",
+                    post_img : "http://media.yogajournal.com/wp-content/uploads/249_hp_groove_09.jpg",
+                    post_video : "",
+                    created_at : new Date('2016-06-29 14:26:16 UTC'),
+                    updated_at : new Date('2016-06-29 14:26:16 UTC')
+                  },
+                  {
+                    id: 2,
+                    title: "Help with my hips!",
+                    description : "I tore my hip flex while I was squatting, does anyone have suggestions on what stretches are best for this?",
+                    post_img : "http://1.bp.blogspot.com/_sCVN1yQuK3A/TNfcYl4FG8I/AAAAAAAADI8/jpc-oc9UaMs/s1600/dhanurasana-posizione-dellarco.jpg",
+                    post_video : "",
+                    created_at : new Date('2016-06-29 14:26:16 UTC'),
+                    updated_at : new Date('2016-06-29 14:26:16 UTC')
+                  },
+                  {
+                    id: 3,
+                    title: "Stretching advice.",
+                    description : "I am a climber and I would like to incorporate stretching into my regiment. What poses are the best for beginners? My arms and back need the most attention.",
+                    post_img : "/images/wheel.jpg",
+                    post_video : "",
+                    created_at : new Date('2016-06-29 14:26:16 UTC'),
+                    updated_at : new Date('2016-06-29 14:26:16 UTC')
+                  }
+              ])
               ]);
             }).then(() => {
               return knex.raw("SELECT setval('posts_id_seq', (SELECT MAX(id) FROM posts));")
