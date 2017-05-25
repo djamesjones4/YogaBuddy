d3d2315c66bd06d0695b53afcc80a13253ba5c32
Derek Jones <djamesjones4@gmail.com>
2017-05-24 18:29:26 -0600

    comments routed

---
+++ routes/comments.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const router = express.Router()
+const knex = require('../knex')
+
+router.get('/:id', (req, res, next) => {
+  knex('yoga_comments')
+    .where({ post_id: req.params.post_id })
+    .then(comments => res.json(comments))
+    .catch(err => next(err))
+})
+
+router.post('/:id', validate, (req, res, next) => {
+  knex('yoga_comments')
+    .insert({ content: req.body.content, post_id: req.params.post_id })
+    .where({ post_id: req.params.post_id })
+    .returning('*')
+    .then(comments => res.json(comments[0]))
+    .catch(err => next(err))
+})
+
+router.patch('/:id', validate, (req, res, next) => {
+  knex('yoga_comments')
+    .update({ content: req.body.content })
+    .where({ post_id: req.params.post_id, id: req.params.id })
+    .returning('*')
+    .then(comments => res.json(comments[0]))
+    .catch(err => next(err))
+})
+
+router.delete('/:id', (req, res, next) => {
+  knex('yoga_comments')
+    .del()
+    .where({ post_id: req.params.post_id, id: req.params.id })
+    .then(() => res.end())
+    .catch(err => next(err))
+})
+
+// function validate(req, res, next) {
+//   const errors = [];
+//   ['content'].forEach(field => {
+//     if (!req.body[field] || req.body[field].trim() === '') {
+//       errors.push({ field, messages: ["cannot be blank"] })
+//     }
+//   })
+//   if (errors.length) return res.status(422).json({ errors })
+//   next()
+// }
+
+module.exports = router
