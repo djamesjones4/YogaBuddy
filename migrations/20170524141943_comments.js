a965ce7a7515e7a3fe4afe2d5ed66dbba75dfe4e
Haley Kalb <haley.kalb@gmail.com>
2017-05-24 18:53:01 -0600

    migrations

--- migrations/20170524141943_comments.js
+++ migrations/20170524141943_comments.js
@@ -4,8 +4,8 @@ exports.up = function(knex) {
     table.boolean('verified').defaultTo(false)
     table.text('comment_text').notNullable().defaultTo('')
     table.text('comment_img').defaultTo('')
-    table.integer('user_id').notNullable().references('users.id').onDelete('CASCADE')
-    table.integer('post_id').notNullable().references('posts.id').onDelete('CASCADE')
+    table.integer('user_id').notNullable().references('yoga_users.id').onDelete('CASCADE')
+    table.integer('post_id').notNullable().references('yoga_posts.id').onDelete('CASCADE')
     table.timestamps(true, true)
   })
 }
