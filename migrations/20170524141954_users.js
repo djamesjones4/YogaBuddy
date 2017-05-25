c27c92b47e9ec1f1616bb482ca5933e2a04a11f7
Haley Kalb <haley.kalb@gmail.com>
2017-05-24 18:48:17 -0600

    migrations

--- migrations/20170524141954_users.js
+++ migrations/20170524141954_users.js
@@ -1,20 +1,20 @@
 exports.up = function(knex) {
   return knex.schema.createTable('users', (table) => {
-    table.increments();
-    table.string('username').notNullable().defaultTo('');
-    table.string('username').notNullable().defaultTo('');
-    table.specificType('password', 'char(255)').notNullable();
-    table.string('email').notNullable().unique();
-    table.text('profile_picture_img').defaultTo('');
-    table.text('bio').defaultTo('');
-    table.boolean('admin').defaultTo(FALSE);
-    table.boolean('moderator').defaultTo(FALSE);
-    table.boolean('professional').defaultTo(FALSE);
-    table.boolean('verified_user').defaultTo(FALSE);
-    table.timestamps(true, true);
-  });
-};
+    table.increments()
+    table.string('username').notNullable().defaultTo('')
+    table.string('username').notNullable().defaultTo('')
+    table.specificType('password', 'char(255)').notNullable()
+    table.string('email').notNullable().unique()
+    table.text('profile_picture_img').defaultTo('')
+    table.text('bio').defaultTo('')
+    table.boolean('admin').defaultTo(false)
+    table.boolean('moderator').defaultTo(false)
+    table.boolean('professional').defaultTo(false)
+    table.boolean('verified_user').defaultTo(false)
+    table.timestamps(true, true)
+  })
+}
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('users');
-};
+  return knex.schema.dropTableIfExists('users')
+}
