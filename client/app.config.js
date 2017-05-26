bbe4f60ac6b965e8f9d07eab15916612ff689c0d
ctaggart27 <ctaggart27@gmail.com>
2017-05-26 00:45:47 -0600

    mergeeriesimpson

--- client/app.config.js
+++ client/app.config.js
@@ -7,21 +7,21 @@
 
     $locationProvider.html5Mode(true) // Setting clean URLs (no hashtags)
     $stateProvider // DEFINING ADDITIONAL STATES FOR Single-Page-Application
-      // .state({ // 1st STATE (on click in this case)
-      //   name: 'splash',
-      //   url: '/',
-      //   component: 'splash'
-      // })
-      // .state({ // main state/component on every page
-      //   name: 'home',
-      //   url: '/home',
-      //   component: 'home'
-      // })
-      // .state({ // 1st STATE (on click in this case)
-      //   name: 'main',
-      //   url: '/om',
-      //   component: 'main'
-      // })
+      .state({ // 1st STATE (on click in this case)
+        name: 'splash',
+        url: '/',
+        component: 'splash'
+      })
+      .state({ // main state/component on every page
+        name: 'home',
+        url: '/home',
+        component: 'home'
+      })
+      .state({ // 1st STATE (on click in this case)
+        name: 'main',
+        url: '/om',
+        component: 'main'
+      })
     .state({ // 1st STATE (on click in this case)
       name: 'posts',
       url: '/posts',
@@ -32,31 +32,31 @@
       url: '/post/:id',
       component: 'onePost'
     })
-    // .state({ // 3rd STATE (on click in this case)
-    //   name: 'post.comment',
-    //   url: '/comments',
-    //   component: 'comments'
-    // })
-    // .state({ // 4th STATE (on click in this case)
-    //   name: 'users',
-    //   url: '/users/:id',
-    //   component: 'users'
-    // })
-    // .state({ // 1st STATE (on click in this case)
-    //   name: 'main.poses',
-    //   url: '/poses',
-    //   component: 'poses'
-    // })
-    // .state({ // 1st STATE (on click in this case)
-    //   name: 'pose',
-    //   url: '/poses/:id',
-    //   component: 'onePose'
-    // })
-    // .state({ // 5th STATE (on click in this case)
-    //   name: 'comment',
-    //   url: '/posts/:id/comments',
-    //   component: 'comments'
-    // })
+    .state({ // 3rd STATE (on click in this case)
+      name: 'post.comment',
+      url: '/comments',
+      component: 'comments'
+    })
+    .state({ // 4th STATE (on click in this case)
+      name: 'users',
+      url: '/users/:id',
+      component: 'users'
+    })
+    .state({ // 1st STATE (on click in this case)
+      name: 'main.poses',
+      url: '/poses',
+      component: 'poses'
+    })
+    .state({ // 1st STATE (on click in this case)
+      name: 'pose',
+      url: '/poses/:id',
+      component: 'onePose'
+    })
+    .state({ // 5th STATE (on click in this case)
+      name: 'comment',
+      url: '/posts/:id/comments',
+      component: 'comments'
+    })
 
   } // END CONFIG FUNCTION
 })()
