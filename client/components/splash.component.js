3f05c16f935f7b9bb7fee427395405027e2f9ea8
Derek Jones <djamesjones4@gmail.com>
2018-01-02 09:33:35 -0700

    preparing demo

--- client/components/splash.component.js
+++ client/components/splash.component.js
@@ -7,20 +7,27 @@
       controller: SplashController
     })
 
-  SplashController.$inject = ['SplashService', '$http', '$stateParams', '$state']
+  SplashController.$inject = ['SplashService', '$http', '$stateParams', '$state', '$interval', '$scope']
 
-  function SplashController(SplashService, $http, $stateParams, $state) {
+  function SplashController(SplashService, $http, $stateParams, $state, $interval, $scope) {
     console.log("you are in the splash Controller")
     const vm = this
     vm.$onInit = onInit
     vm.userAuth = {}
     function onInit() {
       // $(".button-collapse").sideNav()
+      // $interval.cancel(goToFeed)
     }
+
     vm.logIn = function() {
       console.log('userAuth: ', vm.userAuth)
       SplashService.logIn(vm.userAuth)
+    }
 
+    let goToFeed = function() {
+      $interval(function() {
+        $state.go('feed')
+      }, 2500)
     }
 
     vm.moveLotus = function() {
@@ -31,6 +38,9 @@
       lotus_top.className = 'animated-login-top-move move-lotus-top'
       lotus_bottom.className = 'lotus-login-bottom-pulse move-lotus-bottom'
       // wait 2 seconds and then &state.go('feed')
+      goToFeed()
+      $interval.cancel(goToFeed)
+      // TODO: fix recall of function on navigation back
     }
   }
 })()
